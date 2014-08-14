fc3dbdede31e6960ab5784222cdeeb31bcc03f2e
Dirbaio Minikiwi <dirbaio@dirbaio.net>
2014-08-14 23:51:37 +0200

    Fix mobile layout toggle if URL has hash.

--- modules/main/js/page.js
+++ modules/main/js/page.js
@@ -5,6 +5,6 @@ angular.module('app')
 .controller('PageCtrl', function($scope, $window, $cookies, ajax) {
 	$scope.toggleMobileLayout = function(enabled) {
 		$cookies.mobileversion = enabled;
-		$window.location = $window.location;
+		$window.location.reload();
 	}
 })
