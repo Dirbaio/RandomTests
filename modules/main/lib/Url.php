059033d9be4c071009aa035c5447039dbb617efc
Dirbaio Minikiwi <dirbaio@dirbaio.net>
2014-08-16 22:34:42 +0200

    Add support to run pages from the CLI

--- modules/main/lib/Url.php
+++ modules/main/lib/Url.php
@@ -5,17 +5,28 @@ class Url
 
 	public static function getPath()
 	{
-		//Legacy ABXD compat
-		if (!empty($_GET['page']))
+		// Allow running from CLI
+		global $argv;
+		if(php_sapi_name() === 'cli')
+		{
+			if($argv[1])
+				return $argv[1];
+			else
+				return '/';
+		}
+
+		// Legacy ABXD compat
+		if(!empty($_GET['page']))
 			return '/'.$_GET['page'].'.php';
 
-		if (!empty($_SERVER['PATH_INFO']))
+		// Try to figure out the pathinfo
+		if(!empty($_SERVER['PATH_INFO']))
 			return $_SERVER['PATH_INFO'];
 
-		if (!empty($_SERVER['ORIG_PATH_INFO']) && $_SERVER['ORIG_PATH_INFO'] !== '/index.php')
+		if(!empty($_SERVER['ORIG_PATH_INFO']) && $_SERVER['ORIG_PATH_INFO'] !== '/index.php')
 			return $_SERVER['ORIG_PATH_INFO'];
 
-		if (!empty($_SERVER['REQUEST_URI']))
+		if(!empty($_SERVER['REQUEST_URI']))
 			return (strpos($_SERVER['REQUEST_URI'], '?') > 0) ? strstr($_SERVER['REQUEST_URI'], '?', true) : $_SERVER['REQUEST_URI'];
 
 		return '/';
